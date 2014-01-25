56fc264a2046433e927391390c06a6e12e028492
kavitama <jadhavkavita206@gmail.com>
2014-01-25 16:00:35 +0530

    test runs

---
+++ test/libTests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by kavitama on 1/25/14.
+ */
+public class libTests {
+    @Test
+    public void myFirstTest(){
+        assertEquals(1,1);
+    }
+}
